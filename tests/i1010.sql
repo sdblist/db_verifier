60305ab1125fda2420dd696723d003e29c5a0b94
nikonov <vasiliy.nikonov@licens.io>
2024-03-24 15:44:11 +0300

    check_i1010 - extend test with examples

--- tests/i1010.sql
+++ tests/i1010.sql
@@ -13,3 +13,15 @@ INSERT INTO public.i1010_1 (value) VALUES(ARRAY[2, 4, 6]);
 CREATE INDEX i_btree_value ON public.i1010_1 USING btree (value);
 -- i1010 - ok
 CREATE INDEX i_gin_value ON public.i1010_1 USING gin (value);
+
+SET enable_seqscan TO off;
+
+-- Bitmap Index Scan on i_gin_value
+EXPLAIN ANALYZE
+SELECT id FROM public.i1010_1 WHERE value @> ARRAY[1];
+
+-- Index Scan using i_btree_value
+EXPLAIN ANALYZE
+SELECT id FROM public.i1010_1 WHERE value = ARRAY[2, 4, 6];
+
+-- SET enable_seqscan TO on;
